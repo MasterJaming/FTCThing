94507fa87e50f1180abea9104822374f3dfd4471
itisa-series-of-tubes <156371736+itisa-series-of-tubes@users.noreply.github.com>
2024-03-14 16:20:56 -0400

    Update Kim.java
    Updated the English teacher.

--- Kim.java
+++ Kim.java
@@ -4,4 +4,9 @@ public class Kim {
     double weight;
     double height;
 
+    public Person(String name, int age, double weight, double height) {
+    this.name = name;
+    this.age = age;
+    this.weight = weight;
+    this.height = height;}
 }
